05f436877f04d59cdcbd26fb7238784d38fdcd45
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2017-06-24 09:12:54 -0700

    Wrapped the function decode from the encoding utility

--- R/encoding.R
+++ R/encoding.R
@@ -50,12 +50,12 @@ encode <- function(state, b = NA) {
   # Convert to integer vector suitable for C
   xs <- as.integer(state)
 
-  x    <- .C("r_encode_",
-             state   = xs,
-	     n       = as.integer(n),
-	     b       = as.integer(b),
-	     encoded = as.integer(encoded),
-	     err     = as.integer(err))
+  x  <- .C("r_encode_",
+            state   = xs,
+	    n       = as.integer(n),
+	    b       = as.integer(b),
+	    encoded = as.integer(encoded),
+            err     = as.integer(err))
 	    
   if (x$err == 0) {
     encoded <- x$encoded
@@ -64,4 +64,77 @@ encode <- function(state, b = NA) {
   }
 
   encoded
+}
+
+################################################################################
+#' Decode
+#'
+#' Decode an integer into a base-\code{b} array with \code{n} digits. Note that
+#' the base \code{b} must be provided, but the number of digits \code{n} is
+#' optional. If it is provided then the decoded state will have exactly that
+#' many elements. If \code{n} is not provided, the length of the decoded state
+#' is as small as possible.
+#'     
+#' @param encoding Numeric of the encoded state.
+#' @param b Numeric giving the desired base.
+#' @param n Numeric giving the desired number of digits.
+#'
+#' @return Vector giving the decoded state.
+#'
+#' @example inst/examples/ex_encoding_decode.R
+#'
+#' @export
+#'
+#' @useDynLib rinform r_decode_
+################################################################################
+decode <- function(encoding, b, n = NA) {
+  err   <- 0
+  state <- 0
+
+  if (!is.numeric(encoding)) {
+    stop("<encoding> is not numeric")
+  }
+
+  if (is.matrix(encoding)) {
+    stop("<encoding> is a matrix")
+  }
+
+  # Extract length of encoding
+  en <- length(encoding)
+
+  if (en == 0) {
+    stop("cannot use an empty encoding")
+  }
+
+  if (en > 1) {
+    stop("cannot use an array of encodings")
+  }
+  
+  b <- max(2, b)
+
+  if (is.na(n)) {
+    state <- rep(0, times = 32)
+  } else {
+    state <- rep(0, times = n)
+  }
+
+  x <- .C("r_decode_",
+          encoding = as.integer(encoding),
+	  b        = as.integer(b),
+	  state    = as.integer(state),
+	  n        = as.integer(length(state)),
+	  err      = as.integer(err))
+	    
+  if (x$err == 0) {
+    if (!is.na(n)) {
+      state <- x$state
+    } else {
+      # Remove leading zeros
+      state <- x$state[min(which(x$state != 0)):length(x$state)]
+    }
+  } else {
+    stop("inform lib error (", x$err, ")")
+  }
+
+  state
 }
--- src/rinform_encoding.c
+++ src/rinform_encoding.c
@@ -6,3 +6,10 @@ void r_encode_(int *state, int *n, int *b, int *encoded, int *err) {
   *encoded = inform_encode(state, *n, *b, &ierr);
   *err     = ierr;
 }
+
+void r_decode_(int *encoding, int *b, int *state, int *n, int *err) {
+  inform_error ierr = INFORM_SUCCESS;
+    
+  inform_decode(*encoding, *b, state, *n, &ierr);
+  *err = ierr;
+}
