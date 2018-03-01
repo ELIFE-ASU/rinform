13c1d1eac6cdf23281c562c4644e282ec02d2806
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2018-03-01 15:23:12 -0700

    Removed error-raising examples

--- inst/examples/ex_dist_tick.R
+++ inst/examples/ex_dist_tick.R
@@ -4,6 +4,3 @@ d <- tick(d, 1)
 d <- tick(d, 3)
 d # [2, 0, 1, 0]
 
-# These produce an out of bounds error
-d <- tick(d, 0)
-d <- tick(d, 6)
--- inst/examples/ex_shannon_cross_entropy.R
+++ inst/examples/ex_shannon_cross_entropy.R
@@ -4,6 +4,5 @@ shannon_cross_entropy(p, q)               # 1.0000000
 shannon_cross_entropy(q, p)               # Inf
 shannon_cross_entropy(p, q, b = 3)        # 0.6309298
 shannon_cross_entropy(q, p, b = 3)        # Inf
-shannon_cross_entropy(p, q, b = -1)       # Error
-shannon_cross_entropy(q, p, b = -1)       # Error
+
 
