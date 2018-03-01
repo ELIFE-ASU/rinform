08d3abcf7a084dbc28cd644a3c44f84e1f1c936a
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2018-03-01 09:46:11 -0700

    Wrapped and tested series_to_tpm

---
+++ R/seriestotpm.R
@@ -0,0 +1,59 @@
+################################################################################
+# Copyright 2017 ELIFE. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+################################################################################
+
+
+
+################################################################################
+#' Time Series to TPM
+#'
+#' Description..
+#'
+#'
+#' @param series Vector or matrix specifying one or more time series.
+#'
+#' @return Matrix giving the corresponding transition probability matrix.
+#'
+#' @example inst/examples/ex_series_to_tpm.R
+#'
+#' @export
+#'
+#' @useDynLib rinform r_series_to_tpm_
+################################################################################
+series_to_tpm <- function(series) {
+  n   <- 0
+  m   <- 0
+  err <- 0
+
+  .check_series(series)
+
+  # Extract number of series and length
+  if (is.vector(series)) {
+    n <- 1
+    m <- length(series)
+  } else if (is.matrix(series)) {
+    n <- dim(series)[2]
+    m <- dim(series)[1]
+  }
+
+  # Compute the value of <b>
+  b <- max(2, max(series) + 1)
+
+  tpm <- rep(0.0, b * b)
+  x   <- .C("r_series_to_tpm_",
+             series  = as.integer(series),
+	     n       = as.integer(n),
+	     m       = as.integer(m),
+	     b       = as.integer(b),
+	     tpm     = as.double(tpm),
+	     err     = as.integer(err))
+
+  if (.check_inform_error(x$err) == 0) {
+    tpm      <- x$tpm
+    dim(tpm) <- c(b, b)
+  }
+
+  tpm
+}
