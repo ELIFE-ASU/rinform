b5ac867fd829c72b5de3bd139483e06ed231b389
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2018-03-19 17:54:35 -0700

    Deploy gh-pages from travis

--- .push_gh_pages.sh
+++ .push_gh_pages.sh
@@ -3,23 +3,18 @@
 rm -rf out || exit 0;
 mkdir out;
 
-GH_REPO="@github.com/ELIFE-ASU/rinform.git"
-
-FULL_REPO="https://$GH_TOKEN$GH_REPO"
+GH_REPO="ELIFE-ASU/rinform.git"
 
 for files in '*.tar.gz'; do
         tar xfz $files
 done
 
 cd out
-git init
-git config user.name "Gabriele Valentini"
-git config user.email "gabriele.valentini.85@gmail.com"
-git clone https://github.com/ELIFE-ASU/rinform.git
-cd rinform
-git checkout gh-pages
-cp ../../rinform/inst/doc/rinform-vignette.html index.html
-
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git checkout -b gh-pages
+cp ../rinform/inst/doc/rinform-vignette.html index.html
 git add .
-git commit -m "deployed to github pages"
-git push --force --quiet $FULL_REPO master:gh-pages
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git remote add origin-pages https://${GH_TOKEN}@github.com/${GH_REPO} > /dev/null 2>&1
+git push --quiet --set-upstream origin-pages gh-pages 
