beb98bee7e2e43d1eebc11ab1697074159c0be42
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2018-03-21 19:46:53 -0700

    Done with CRAN fixes

--- src/rinform_black_box.c
+++ src/rinform_black_box.c
@@ -9,7 +9,8 @@
 void r_black_box_(int *series, int *l, int *n, int *m, int *b, int *r, int *rNull,
 		  int *s, int *sNull, int *box, int *err) {
   inform_error ierr = INFORM_SUCCESS;
-  size_t R[*l], S[*l];
+  size_t *R = (size_t *) R_alloc(*l, sizeof(size_t));
+  size_t *S = (size_t *) R_alloc(*l, sizeof(size_t));
 
   for (size_t i = 0; i < *l; ++i) {
     if (*rNull == 0) { R[i] = r[i]; }
